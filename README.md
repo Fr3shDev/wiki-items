ec93688abd3ce2c3105f64ade4092efd64d94636
Wandji69 <collinschuwa@gmail.com>
2024-07-27 12:54:58 +0100

    Update file structure to meet the recommended structure.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Documentation for the Server
+
+- <https://www.npmjs.com/package/passport-mediawiki-oauth>
+- <https://wikitech.wikimedia.org/wiki/Help:Toolforge/My_first_NodeJS_OAuth_tool>
---
+++ js/config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+config.session_secret = "generate some random value here";
+config.consumer_key = "the consumer token value from your OAuth consumer registration";
+config.consumer_secret = "the secret token value from your OAuth consumer registration";
+
+module.exports = config;
