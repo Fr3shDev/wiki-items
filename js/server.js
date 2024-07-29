aa430cf0d59c4e6ed60c07f554f06f27596afab9
LeonardAzah <isongleonard@gmail.com>
2024-07-29 12:18:21 +0100

    adding cors

--- js/server.js
+++ js/server.js
@@ -22,8 +22,11 @@ require("dotenv").config();
 const notFound = require("./exception-handlers/not-found");
 const errorHandlerMiddleware = require("./exception-handlers/error-handler");
 var express = require("express");
+const cors = require("cors");
 
 var app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -37,7 +40,7 @@ app.use("/property-value", searchRoute);
 app.use("/api/v1/auth", authRoute);
 
 app.use(notFound);
-app.use(errorHandlerMiddleware);
+app.use(errorHandlerMiddleware); //make sure very routes is above this middleware
 
 app.listen(process.env.PORT || 8000, function () {
   console.log("Node.js app listening on port 8000!");
